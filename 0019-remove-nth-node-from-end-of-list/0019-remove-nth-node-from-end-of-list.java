9e9608e967518aaac51e96facc7dbc4d137160dd
kartikpareek1115 <kartikpareek115@gmail.com>
2026-08-22 09:24:42 +0530

    Time: 0 ms (100%), Space: 43.5 MB (40.4%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -13,27 +13,49 @@ class Solution {
         if(head == null ){
             return head;
         }
-        ListNode temp = head;
-        int count = 0;
-        while(temp != null){
-            count++;
-            temp = temp.next;
-        }
-        if(count == n){
-            return head.next;
-        }
-        int res = count - n;
-        temp = head;
-        while(res != 0){
-            res--;
-            
-            if(res == 0){
-               break;
-            }
-            temp = temp.next;
-        }
-         temp.next = temp.next.next;
 
-         return head;
+        //Brute - force
+        // ListNode temp = head;
+        // int count = 0;
+        // while(temp != null){
+        //     count++;
+        //     temp = temp.next;
+        // }
+        // if(count == n){
+        //     return head.next;
+        // }
+        // int res = count - n;
+        // temp = head;
+        // while(res != 0){
+        //     res--;
+            
+        //     if(res == 0){
+        //        break;
+        //     }
+        //     temp = temp.next;
+        // }
+        //  temp.next = temp.next.next;
+
+        //  return head;
+
+
+        //Optimal -- fast and slow pointer
+        ListNode slow = head;
+        ListNode fast = head;
+        for(int i = 0; i<n ; i++){
+            fast = fast.next;
+        }
+
+        if (fast == null) {
+    return head.next;
+}
+         
+        while(fast != null && fast.next != null  ){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+
     }
 }
