09cb4cce07f1b4e01eb05d69bbc4bde9ad3da61c
kartikpareek1115 <kartikpareek115@gmail.com>
2026-08-23 09:41:05 +0530

    Time: 10 ms (14.08%), Space: 98.9 MB (23.47%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,26 +9,67 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        ListNode temp = head;
-        int i = 0;
-        int count = 0;
-        while(temp != null){
-            arr.add(temp.val);
-            i++;
-            temp = temp.next;
-           count++;
+      public ListNode reverse(ListNode head) {
+        // Check if the list is empty or has only one node
+        if (head == null || head.next == null) {
+            return head;  // No change is needed; return the current head
         }
-        temp = head;
-        i = count-1;
-        while(temp != null){
-            if(arr.get(i) != temp.val){
+
+        // Recursive step: Reverse the remaining part of the list and get the new head
+        ListNode NewHead = reverse(head.next);
+
+        // Store the next node in 'front' to reverse the link
+        ListNode front = head.next;
+
+        // Update the 'next' pointer of 'front' to point to the current head
+        front.next = head;
+
+        // Set the 'next' pointer of the current head to null to break the original link
+        head.next = null;
+
+        // Return the new head obtained from the recursion
+        return NewHead;
+    }
+    public boolean isPalindrome(ListNode head) {
+        // ArrayList<Integer> arr = new ArrayList<>();
+        // ListNode temp = head;
+        // int i = 0;
+        // int count = 0;
+        // while(temp != null){
+        //     arr.add(temp.val);
+        //     i++;
+        //     temp = temp.next;
+        //    count++;
+        // }
+        // temp = head;
+        // i = count-1;
+        // while(temp != null){
+        //     if(arr.get(i) != temp.val){
+        //         return false;
+        //     }
+        //     temp = temp.next;
+        //     i--;
+        // }
+        // return true;
+
+        //Optimal
+        if(head == null || head.next == null) return true;
+        ListNode slow = head, fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode NewHead = reverse(slow.next);
+        ListNode first = head , second = NewHead;
+        while(second != null){
+            if(first.val != second.val){
+                reverse(NewHead);
                 return false;
             }
-            temp = temp.next;
-            i--;
+            first = first.next;
+            second = second.next;
         }
+        reverse(NewHead);
         return true;
     }
 }
