fa0a73ffdda4f0cba31260f89d82a72171b07f58
kartikpareek1115 <kartikpareek115@gmail.com>
2026-08-27 07:54:42 +0530

    Time: 1 ms (99.91%), Space: 52.3 MB (98.2%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -10,22 +10,52 @@
  * }
  */
 public class Solution {
+     ListNode collisionPoint(ListNode t1,ListNode t2, int d){
+            while(d>0){
+                d--;
+                t2 = t2.next;
+            }
+            while(t1 != t2){
+                t1 = t1.next;
+                t2 = t2.next;
+            }
+            return t1;
+        }
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashMap<ListNode, Integer> map = new HashMap<>();
 
-        ListNode  temp = headA;
-        while(temp != null){
-            map.put(temp, 1);
-            temp = temp.next;
-        } 
-        temp = headB;
-        while(temp != null){
-           if( map.containsKey(temp)) {
-            return temp;
-           }
-           temp = temp.next;
+        //Brute Force
+        // HashMap<ListNode, Integer> map = new HashMap<>();
+        // ListNode  temp = headA;
+        // while(temp != null){
+        //     map.put(temp, 1);
+        //     temp = temp.next;
+        // } 
+        // temp = headB;
+        // while(temp != null){
+        //    if( map.containsKey(temp)) {
+        //     return temp;
+        //    }
+        //    temp = temp.next;
+        // }
+        // return null;
+
+        //Optmial _ 1
+
+       
+        ListNode temp1 = headA, temp2 = headB;
+        int n1 = 0, n2  = 0;
+        while(temp1 != null){
+            n1++;
+            temp1 = temp1.next;
+        }
+        while(temp2 != null){
+            n2++;
+            temp2 = temp2.next;
         }
 
-        return null;
+        if(n1<n2){
+            return collisionPoint(headA, headB, n2-n1);
+        }
+        else return collisionPoint(headB, headA, n1-n2);
     }
 }
