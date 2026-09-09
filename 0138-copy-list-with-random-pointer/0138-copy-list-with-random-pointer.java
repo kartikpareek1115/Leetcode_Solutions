ad6174ced927b2623af03568bf77fb09128c9ecf
kartikpareek1115 <kartikpareek115@gmail.com>
2026-09-09 11:53:18 +0530

    Time: 0 ms (100%), Space: 46.5 MB (71.65%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -17,36 +17,82 @@ class Solution {
     public Node copyRandomList(Node head) {
         if(head == null) return null;
 
-        Node temp = head;
-        ArrayList<Node> copy = new ArrayList<>();
+        //Brute-Force
+        // Node temp = head;
+        // ArrayList<Node> copy = new ArrayList<>();
 
-        while(temp != null){
-            copy.add(new Node(temp.val));
-            temp = temp.next;
-        }
+        // while(temp != null){
+        //     copy.add(new Node(temp.val));
+        //     temp = temp.next;
+        // }
 
-        temp = head;
-        int i = 0;
+        // temp = head;
+        // int i = 0;
 
-        while (temp != null){
+        // while (temp != null){
             
-            if(temp.next != null){
-                copy.get(i).next = copy.get(i+1);
-            }
+        //     if(temp.next != null){
+        //         copy.get(i).next = copy.get(i+1);
+        //     }
 
-            if(temp.random != null){
-                Node randomTemp = head;
-                int j = 0;
-                while(randomTemp != temp.random){
-                    randomTemp = randomTemp.next;
-                    j++;
-                }
-                copy.get(i).random = copy.get(j); 
-            }
-            temp = temp.next;
-            i++;
+        //     if(temp.random != null){
+        //         Node randomTemp = head;
+        //         int j = 0;
+        //         while(randomTemp != temp.random){
+        //             randomTemp = randomTemp.next;
+        //             j++;
+        //         }
+        //         copy.get(i).random = copy.get(j); 
+        //     }
+        //     temp = temp.next;
+        //     i++;
            
+        // }
+        // return copy.get(0);
+
+
+       //Optimal
+       insertCopyInBetween(head);
+       connectRandomPointers (head);
+       return retreiveList(head);
+    
+    }
+    void insertCopyInBetween(Node head){
+        Node temp = head;
+        while(temp != null){
+            Node nextElement = temp.next;
+            Node copy = new Node(temp.val);
+            copy.next = nextElement;
+            temp.next = copy;
+            temp = nextElement;
         }
-        return copy.get(0);
+    }
+
+    void connectRandomPointers (Node head){
+        Node temp = head;
+        while(temp != null){
+            Node copyNode = temp.next;
+            if(temp.random != null){
+                copyNode.random = temp.random.next;
+            }
+            else{
+                copyNode.random =  null;
+            }
+            temp = temp.next.next;
+        }
+    }
+
+    Node retreiveList(Node head){
+        Node dummy = new Node(-1);
+        Node temp = head;
+        Node res = dummy;
+        while (temp != null){
+           res.next = temp.next;
+           res = res.next;
+
+           temp.next = temp.next.next;
+           temp = temp.next;
+        }
+        return dummy.next;
     }
 }
